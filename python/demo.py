ff23dfd33668eaca9c77f17019cec5e224927759
niovl <niovanln@gmail.com>
2021-01-08 14:16:12 +0800

    Update demo.py

--- python/demo.py
+++ python/demo.py
@@ -1,36 +1,7 @@
 
 from json import load
 import cv2 as cv
-from os.path import splitext, join, exists
-from os import walk
 import numpy as np
-
-def file_ignore_suffix(files):
-    lst = list()
-    for f in files:
-        fn, ext = splitext(f)
-        lst.append(fn)
-    lst = np.unique(lst).tolist()
-    use = list()
-    for f in lst:
-        if exists(f + '.jpg') and exists(f + '.json'):
-            use.append(f)
-        else:
-            print('{}: image and label did not exist at the same time.'.format(f))
-    return lst
-
-
-def file_list(file_dir, suffix):
-    files_ = list()
-    for root, dirs, files in walk(file_dir):
-        if len(files) == 0:
-            continue
-        for f in files:
-            fn, ext = splitext(f)
-            if ext in suffix:
-                # files_.append(join(root, f).replace(file_dir + '/', ''))
-                files_.append(join(root, f))
-    return files_
 
 
 def load_labelme(file_path):
@@ -55,8 +26,9 @@ def load_image(file_path):
 
 
 def main():
+    # Customize the way to load ROI here, E.g. labelme
     inp = load_labelme('../images/kite.json')
-    print(len(inp))
+    # print(len(inp))
     ori = cv.imread('../images/kite.jpg')
     tool = RectanglesLayout()
     tool.infer(inp)
