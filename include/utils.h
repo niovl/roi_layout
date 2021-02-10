691ce9fed3585c6cd6f5e3ccced0157a3a59051c
niovl <niovanln@gmail.com>
2021-02-10 16:32:30 +0800

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -8,25 +8,27 @@
 using namespace std;
 using namespace cv;
 
-template < typename T> vector<size_t>  sort_indexes(const vector<T>  &v);
+
+template < typename Dtype> vector<size_t>  sort_indexes(const vector<Dtype>  &v);
 float get_v(int a, int b);
 void adjust_bounds(Rect &box, Rect ref);
-float iou(Rect r0, Rect r1);
+
+template<typename Dtype> static inline
+double intersectionOverUnion(const Rect_<Dtype>& a, const Rect_<Dtype>& b);
+
+
+template<typename Dtype> static inline
+double generalizedIntersectionOverUnion(const Rect_<Dtype>& a, const Rect_<Dtype>& b);
+
+
+template<typename Dtype> static inline
+double distancIntersectionOverUnion(const Rect_<Dtype>& a, const Rect_<Dtype>& b);
+
+
+template<typename Dtype> static inline
+double completeIntersectionOverUnion(const Rect_<Dtype>& a, const Rect_<Dtype>& b);
 
 
 
-template < typename T> 
-vector< size_t>  sort_indexes(const vector< T>  &v) {
-
-  // initialize original index locations
-  vector< size_t>  idx(v.size());
-  for (size_t i = 0; i != idx.size(); ++i) idx[i] = i;
-
-  // sort indexes based on comparing values in v
-  sort(idx.begin(), idx.end(),
-       [& v](size_t i1, size_t i2) {return v[i1] >  v[i2];});
-
-  return idx;
-}
 
 #endif   // UTILS_H_
